ad4b34f6ad967ab8409c7dc6de38510566fec71f
Rajneesh Goyal <156995340+RajneeshGoyal25@users.noreply.github.com>
2024-10-10 00:55:10 -0400

    Add files via upload

---
+++ solved_challenges.js
@@ -0,0 +1,17 @@
+// Challenge 1***
+
+const getCelsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
+console.log(getCelsius(32));
+console.log(`The temperature is ${getCelsius(32)} \xB0C`);
+
+// Challenge 2***
+
+const minMax = (arr) => ({min: Math.min(...arr), max: Math.max(...arr)});
+console.log(minMax([1, 2, 3, 4, 5]));
+
+//Challenge 3***
+
+(function(length, width){
+    const area = length * width;
+    console.log(`The area of a rectangle with a length of ${length} and a width of ${width} is ${area}.`);
+})(10, 5);
